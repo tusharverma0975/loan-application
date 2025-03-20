07b3c41d22f36becf94fe0ba8267d1e9afab763d
tusharverma0975 <tushar.verma0975@gmail.com>
2025-03-20 23:19:57 +0530

    not used router outlet removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 import { MainComponentComponent } from "./main-component/main-component.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, MainComponentComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  selector: "app-root",
+  imports: [MainComponentComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'loan-application';
+  title = "loan-application";
 }
